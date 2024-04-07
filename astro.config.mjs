102a78763cfec2be3b14fe6b298f37370170c687
addiveloper <addi.veloper@gmail.com>
2024-04-07 10:37:52 -0400

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en", "es"]
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
